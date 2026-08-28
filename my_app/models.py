5dc1c5ab695286956284f49ec3a4dabdd6d8e442
Elmurod <elmurodmarimov7655@gmail.com>
2026-08-28 12:31:24 +0500

    add register to models

--- my_app/models.py
+++ my_app/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Person(models.Model):
+    first_name = models.CharField(max_length=250)
+    last_name = models.CharField(max_length=250)
+    email = models.EmailField(max_length=250)
+    company = models.CharField(max_length=250)
+    phone = models.CharField(max_length=12)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
 class PredictionHistory(models.Model):
     """Foydalanuvchilar yuklagan rasmlar va AI tahlil natijalari tarixi"""
     # Foydalanuvchi tizimga kirmagan bo'lsa ham ishlatish uchun null=True qilamiz
